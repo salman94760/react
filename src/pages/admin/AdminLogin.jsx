e12051378dc5aecf6a6ad79f109c8faf6a33790c
salman <salman94760@gmail.com>
2026-02-22 15:21:32 +0530

    structure and flow change

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -25,15 +25,15 @@ const AdminLogin = ()=>{
 			<div className="grid grid-cols-2">
 				<div className="text-black text-center h-screen content-center"><h2 className="text-[100px] text-italic italic font-medium font-[roboto]">Login</h2></div>
     			<div className="text-black text-center content-center">
-    				<form onSubmit={handleSubmit} className="max-w-sm mx-auto border-2 border-grey-500 rounded p-8">
+    				<form onSubmit={handleSubmit} className="max-w-sm mx-auto border-2 border-gray-500 rounded p-8">
 
 						<div className="mb-5">
-							<label for="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email *</label>
+							<label htmlFor="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email *</label>
     					<input value={formData.email} onChange={handleChange} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
   					</div>
 
   					<div className="mb-5">
-    					<label for="password" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password *</label>
+    					<label htmlFor="password" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password *</label>
     					<input value={formData.password} onChange={handleChange} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter password" required />
   					</div>
 
@@ -41,8 +41,8 @@ const AdminLogin = ()=>{
     					<div className="flex items-center h-5">
       					<input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
     					</div>
-    					<label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-    					<label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Not registered? <Link className="text-red-500 underline" to="/admin/register">Register now.</Link></label>
+    					<label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+    					<label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Not registered? <Link className="text-red-500 underline" to="/admin/register">Register now.</Link></label>
   					</div>
   					
   					<button type="submit" className="text-left text-white bg-black hover:bg-white hover:text-black hover:border hover:border-black py-2 px-4 rounded">Submit</button>
--- src/pages/admin/AdminRegister.jsx
+++ src/pages/admin/AdminRegister.jsx
@@ -39,29 +39,29 @@ const AdminRegister = ()=>{
 			<div className="grid grid-cols-2">
 				<div className="text-black text-center h-screen content-center"><h2 className="text-[100px] text-italic italic font-medium font-[roboto]">Register</h2></div>
     			<div className="text-black text-center content-center">
-    				<form onSubmit={handleSubmit} className="max-w-sm mx-auto border-2 border-grey-500 rounded p-8">
+    				<form onSubmit={handleSubmit} className="max-w-sm mx-auto border-2 border-gray-500 rounded p-8">
     					<div className="mb-5">
-    						<label for="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fullname *</label>
+    						<label htmlFor="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fullname *</label>
     						<input value={formData.fullname} onChange={handleChange} type="text" id="fullname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Full name" required />
   						</div>
 
   						<div className="mb-5">
-    						<label for="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email *</label>
+    						<label htmlFor="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email *</label>
     						<input value={formData.email} onChange={handleChange} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required />
   						</div>
 
   						<div className="mb-5">
-    						<label for="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone *</label>
+    						<label htmlFor="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone *</label>
     						<input value={formData.phone} onChange={handleChange} type="text" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Phone" required />
   						</div>
 
   						<div className="mb-5">
-    						<label for="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
-    							<textarea value={formData.address} onChange={handleChange} id="address" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+    						<label htmlFor="email" className="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+    							<textarea value={formData.address} onChange={handleChange} id="address" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
   						</div>
 
   						<div className="mb-5 text-left">
-  							<label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Have an account? <Link className="text-red-500 underline" to="/admin/login">Sign in</Link></label>
+  							<label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Have an account? <Link className="text-red-500 underline" to="/admin/login">Sign in</Link></label>
   						</div>
   						<button type="submit" className="text-left text-white bg-black hover:bg-white hover:text-black hover:border hover:border-black py-2 px-4 rounded">Register</button>
   					</form>
